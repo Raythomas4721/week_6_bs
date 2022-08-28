404344e225891607c1ca633597c2766ef6c4ea18
Ray <ray123321123321@gmail.com>
2022-08-27 22:54:20 -0700

    first commit

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,25 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+$('.ham-icon').click(function(e) {
+  e.preventDefault();
+  $('.dropdown').toggleClass('active')
+})
+
+var swiper = new Swiper(".swiper-program", {
+  autoplay: {
+      disableOnInteraction: false,
+      delay: 0
+  },
+  speed: 2000,
+  loop: true,
+  spaceBetween: 16,
+  slidesPerView: 3,
+  //多欄
+  breakpoints: {
+      768: {
+          slidesPerView: 6
+      },
+      992: {
+          slidesPerView: 9
+      }
+  }
+
 });
